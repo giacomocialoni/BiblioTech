d3cce175e57c648bb1ace13d87e2282b03cf4580
Giacomo Cialoni <giacomo.cialoni@gmail.com>
2026-01-05 19:35:40 +0100

    Updated Info controller gui to remove sonar issues

--- src/main/java/controller/gui/InfoControllerGUI.java
+++ src/main/java/controller/gui/InfoControllerGUI.java
@@ -2,7 +2,11 @@ package controller.gui;
 
 import app.state.StateManager;
 
+// Questo è uno stato statico e non effettua azioni
+
 public class InfoControllerGUI {
     public void setStateManager(StateManager stateManager) {
+    	
+    	//usa state manager se in futuro ce ne saraà bisogno
     }
 }
